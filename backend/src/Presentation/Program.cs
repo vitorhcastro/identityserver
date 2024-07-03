8033276e296ec32cc4341079e38cf6f7df17dbac
Vitor Castro <vitorhugocastro95@gmail.com>
2024-07-03 17:41:54 +0100

    moved stuff around

--- backend/src/Presentation/Program.cs
+++ backend/src/Presentation/Program.cs
@@ -9,9 +9,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -24,7 +21,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
-app.MapFallbackToFile("/index.html");
 
 app.Run();
